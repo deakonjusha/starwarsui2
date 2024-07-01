44ee37529bf765e429b592559eb73a0ac1cf254a
Angie Chen <alice@webage.com>
2024-07-01 13:37:15 -0400

    added some urls

--- StarWarsTeam1-main/server/server.js
+++ StarWarsTeam1-main/server/server.js
@@ -35,8 +35,51 @@ app.get("/api/characters", async (req, res) => {
   res.json(characters);
 });
 
+app.get("/api/films/:id/characters", async (req, res) => {
+  let id = parseInt(req.params.id);
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection = db.collection("films_characters");
+  const characters = await collection.find({ film_id: id }).toArray();
+  res.json(characters);
+});
+
+app.get("/api/films/:id/planets", async (req, res) => {
+  let id = parseInt(req.params.id);
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection = db.collection("films_planets");
+  const planets = await collection.find({ film_id: id }).toArray();
+  res.json(planets);
+});
+
+app.get("/api/characters/:id/films", async (req, res) => {
+  let id = parseInt(req.params.id);
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection = db.collection("films_characters");
+  const films = await collection.find({ character_id: id }).toArray();
+  res.json(films);
+});
+
+app.get("/api/planets/:id/films", async (req, res) => {
+  let id = parseInt(req.params.id);
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection = db.collection("films_planets");
+  const films = await collection.find({ planet_id: id }).toArray();
+  res.json(films);
+});
+
+app.get("/api/planets/:id/characters", async (req, res) => {
+  let id = parseInt(req.params.id);
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection = db.collection("characters");
+  const characters = await collection.find({ homeworld: id }).toArray();
+  res.json(characters);
+});
+
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
-
-
